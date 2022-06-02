fdce708bbbe8a8a04606c793c74a39eb7951b139
Your Name <you@example.com>
2022-06-02 23:38:29 +0200

    override equals() for Marble

--- src/Marble.java
+++ src/Marble.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Marble {
     int owner; // 0->No owner, 1->Player 1, 2->Player 2.
     int color;
@@ -17,5 +19,13 @@ public class Marble {
 
     public void setValid(boolean valid) {
         isValid = valid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Marble marble = (Marble) o;
+        return owner == marble.owner && color == marble.color && isValid == marble.isValid;
     }
 }
